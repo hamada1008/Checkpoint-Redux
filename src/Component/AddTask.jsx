353924a8d8f9f54780ed8c089ab51b0d55d6ae5c
Oussama <oussamahamada@yahoo.com>
2021-08-16 00:44:22 +0100

    final

--- src/Component/AddTask.jsx
+++ src/Component/AddTask.jsx
@@ -1,67 +1,83 @@
-import React , {useState} from 'react'
-import Task from "./Task"
-import { useSelector, useDispatch } from 'react-redux'
-import { addnote , filterminus,filterreset, removenote} from '../Redux/actions'
-import tasklist from './ListTask'
+import React, { useState } from "react";
+import Task from "./Task";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addnote,
+} from "../Redux/actions";
 
 const AddTask = () => {
+  const items = useSelector((state) => state.bigReducer);
+  const [fillter, setFillter] = useState(false);
+  const dispatch = useDispatch();
 
-    const items = useSelector(state => state.bigReducer)
-    const [fillter,setFillter] = useState(false)
-    const dispatch = useDispatch()
-
-    
-
-
-
-
-    function addtask(e) {
-
-       e.target.previousSibling.value !==''&& dispatch(addnote(e.target.previousSibling.value))
-       e.target.previousSibling.value = ''
+  function addtask(e) {
+    e.target.previousSibling.value !== "" &&
+      dispatch(addnote(e.target.previousSibling.value));
+    e.target.previousSibling.value = "";
+  }
+  function addtask2(e) {
+    if (e.key === "Enter" && e.target.value !== "") {
+      dispatch(addnote(e.target.value));
+      e.target.value = "";
     }
-    function addtask2(e) {
-        if (e.key === 'Enter' && e.target.value !== "") {
-        dispatch(addnote(e.target.value))
-        e.target.value = ''
+  }
+
+  function filterTask(e) {
+    switch (e.target.value) {
+      case "Done":
+        setFillter(true);
+        break;
+      case "All":
+        setFillter(false);
+        break;
+      default:
     }
-    }
-    
+  }
 
-    function filterTask (e) {
-        
-        switch (e.target.value) {
-            
-            case "Done" :  setFillter(true); break;
-            case 'All':  setFillter(false); break;
-            default: 
-        }
-        
-    }
+  return (
+    <div className="List">
+      <header>
+        <h1>To-Do-List</h1>
+        <input
+          type="text"
+          placeholder="Add a task to do"
+          onKeyPress={addtask2}
+        />
+        <button className="addbtn" onClick={addtask}>
+          +
+        </button>
+        <select className="filter" name="filter" onChange={filterTask}>
+          <option value="All">All</option>
+          <option value="Done">Done</option>
+        </select>
+      </header>
+      {fillter
+        ? items
+            .filter((el) => el.isDone === true)
+            .map(
+              (el) =>
+                el.description !== "" && (
+                  <Task
+                    id={el.id}
+                    key={el.id}
+                    desc={el.description}
+                    dn={el.isDone}
+                  />
+                )
+            )
+        : items.map(
+            (el) =>
+              el.description !== "" && (
+                <Task
+                  id={el.id}
+                  key={el.id}
+                  desc={el.description}
+                  dn={el.isDone}
+                />
+              )
+          )}
+    </div>
+  );
+};
 
-
-    return (
-        <div className="List">
-           <header>
-           <h1>To-Do-List</h1>
-          <input type="text" placeholder="Add a task to do" onKeyPress={addtask2}/>
-          <button className="addbtn" onClick={addtask}  >+</button>
-          <select className="filter" name='filter' onChange={filterTask}>
-              <option value="All">All</option>
-              <option value="Done">Done</option>
-          </select>
-          </header>
-            {fillter ? items.filter (el => el.isDone=== true).map(el => el.description !=="" &&   <Task 
-                                    id = {el.id}
-                                    key={el.id}
-                                    desc={el.description}
-                                    dn = {el.isDone} />) : items.map(el => el.description !=="" &&   <Task 
-                                    id = {el.id}
-                                    key={el.id}
-                                    desc={el.description}
-                                    dn = {el.isDone} />) }   
-        </div>
-    )
-}
-
-export default AddTask
+export default AddTask;
